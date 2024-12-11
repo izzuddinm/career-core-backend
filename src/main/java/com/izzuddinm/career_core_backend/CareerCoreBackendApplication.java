ceb4565204b676667ac9675ccaa1957946f6ef85
Muhammad Ayom Izzuddin <ayom.izzudin@gtech.digital>
2024-12-11 09:53:52 +0700

    initial commit career core backend

---
+++ src/main/java/com/izzuddinm/career_core_backend/CareerCoreBackendApplication.java
@@ -0,0 +1,13 @@
+package com.izzuddinm.career_core_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CareerCoreBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CareerCoreBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/izzuddinm/career_core_backend/CareerCoreBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.izzuddinm.career_core_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CareerCoreBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
